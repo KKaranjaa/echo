23520fa08f068cbe143046292d32f65e0268eccb
KKaranjaa <alvinkaranja099@gmail.com>
2026-07-16 13:00:18 +0300

    Fix: Limit Celery to 1 worker to prevent OOM on Render

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start Celery worker in the background
-celery -A echo worker --loglevel=info &
+celery -A echo worker --loglevel=info --concurrency=1 &
 
 # Start Gunicorn in the foreground
 exec gunicorn echo.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --timeout 120
